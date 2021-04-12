0240dd433096414c9969c100ac50d8888043e603
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2021-04-12 13:37:31 -0700

    Finished the first step

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    text: 'kfhjksdf'
+  };
+
+  textChangeHandler = (event) => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <input type="text" onChange={this.textChangeHandler} value={this.state.text}/>
+      </div>
+    );
+  };
 }
 
 export default App;
---
+++ assignment2/src/Validation/validation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Validation = () => {
+    return (
+        <div className="Validation">
+            
+        </div>
+    );
+};
+
+export default Validation;
